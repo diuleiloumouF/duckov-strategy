d41853cc9ce2dbda41ebad43fcbb8a9716477fca
BeSmile <l735465519@gmail.com>
2025-11-08 20:00:33 +0800

    fix: 修复header语言包错误

--- proxy.ts
+++ proxy.ts
@@ -1,13 +1,17 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { getLocale, setCookie } from '@/app/actions/cookies';
+import { getLocale, removeCookie, setCookie } from '@/app/actions/cookies';
 import { defaultLanguage } from '@/app/i18n/config';
 import { LANG_KEY } from '@/app/constants';
 
 // This function can be marked `async` if using `await` inside
 export async function proxy(request: NextRequest) {
-    const language = request.headers.get('accept-language') || defaultLanguage;
+    const langs = request.headers.get('accept-language') || defaultLanguage;
+    const language = langs?.split(",")?.[0] || defaultLanguage;
     const cookieLocale = await getLocale();
+    if (cookieLocale.indexOf(",")) {
+        await removeCookie(LANG_KEY);
+    }
     await setCookie(LANG_KEY, cookieLocale || language);
 
     return NextResponse.next();
